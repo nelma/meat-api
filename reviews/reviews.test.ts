8d82fb7f25743df80dc1c12633ab9dff1679d35f
nelma <nelmasb@gmail.com>
2018-10-22 22:30:31 -0200

    criando mais testes

--- reviews/reviews.test.ts
+++ reviews/reviews.test.ts
@@ -1,8 +1,8 @@
 import 'jest';
 import * as request from 'supertest';
-import { environment } from '../common/environment';
 
 import { beforeAllTests, afterAllTests  } from '../jest.startup';
+import * as mongoose from 'mongoose';
 
 let address: string = (<any>global).address;
 
@@ -17,4 +17,38 @@ test('get /reviews', () => {
             expect(response.body.items).toBeInstanceOf(Array)
           })
           .catch(fail)
+})
+
+test('get /reviews/aaaaa - not found', ()=>{
+  return request(address)
+         .get('/reviews/aaaaa')
+         .then(response=>{
+           expect(response.status).toBe(404)
+         })
+         .catch(fail)
+})
+
+/*
+  Exemplo de como pode ser um post para reviews
+*/
+
+test('post /reviews', ()=>{
+  return request(address)
+            .post('/reviews')
+            .send({
+              date: '2018-02-02T20:20:20',
+              rating: 4,
+              comments: 'ok',
+              user: new mongoose.Types.ObjectId(),
+              restaurant: new mongoose.Types.ObjectId()
+            })
+            .then(response=>{
+              expect(response.status).toBe(200)
+              expect(response.body._id).toBeDefined()
+              expect(response.body.rating).toBe(4)
+              expect(response.body.comments).toBe('ok')
+              expect(response.body.user).toBeDefined()
+              expect(response.body.restaurant).toBeDefined()
+            })
+            .catch(fail)
 })
--- users/users.test.ts
+++ users/users.test.ts
@@ -1,12 +1,8 @@
 import 'jest';
 import * as request from 'supertest';
 
-import {Server} from '../server/server';
-import {environment} from '../common/environment';
-import {userRouter} from './users.router';
-import {User} from './users.model';
-
 import { beforeAllTests, afterAllTests } from '../jest.startup';
+import { response } from 'spdy';
 
 let address: string = (<any>global).address;
 
@@ -44,6 +40,81 @@ test('post /users', () => {
     }).catch(fail)
 })
 
+test('post /user - nome obrigatorio', () => {
+  return request(address)
+  .post('/users')
+  .send({
+    email: 'usuario-obrigatorio@email.com',
+    password: '123456',
+    cpf: '999.999.999-99'
+  })
+  .then(response => {
+    expect(response.status).toBe(400)
+    expect(response.body.errors).toBeInstanceOf(Array);
+    expect(response.body.errors[0].message).toContain('name');
+  }).catch(fail)
+})
+
+test('post /users - cpf invalido', ()=>{
+  return request(address)
+            .post('/users')
+            .send({
+              name: 'usuario 12',
+              email: 'user12@gmail.com',
+              password: '123456',
+              cpf: '675.028.222-93'
+            })
+            .then(response=>{
+              expect(response.status).toBe(400)
+              expect(response.body.errors).toBeInstanceOf(Array)
+              expect(response.body.errors).toHaveLength(1)
+              expect(response.body.errors[0].message).toContain('Invalid CPF')
+            })
+            .catch(fail)
+})
+
+test('post /users - email duplicado', ()=>{
+  return request(address)
+            .post('/users')
+            .send({
+              name: 'dupe',
+              email: 'dupe@gmail.com',
+              password: '123456',
+              cpf: '593.436.344-12'
+            }).then(response=>
+                   request(address)
+                      .post('/users')
+                      .send({
+                        name: 'dupe',
+                        email: 'dupe@gmail.com',
+                        password: '123456',
+                        cpf: '593.436.344-12'
+                      }))
+            .then(response=>{
+              expect(response.status).toBe(400)
+              expect(response.body.message).toContain('E11000 duplicate key')
+            })
+            .catch(fail)
+})
+
+test('get /users - findByEmail', () => {
+  return request(address)
+    .post('/users')
+    .send({
+      name: 'usuario 3',
+      email: 'usuario3@email.com',
+      password: '123456',
+    }).then(response => request(address)
+              .get('/users')
+              .query({email: 'usuario3@email.com'}))
+      .then(response => {
+        expect(response.status).toBe(200)
+        expect(response.body.items).toBeInstanceOf(Array)
+        expect(response.body.items).toHaveLength(1)
+        expect(response.body.items[0].email).toBe('usuario3@email.com')
+      })
+})
+
 test('get /users/aaaa - not found', () => {
   return request(address)
   .get('/users/aaaa')
@@ -74,4 +145,76 @@ test('patch /users/:id', () => {
       expect(response.body.password).toBeUndefined()
     })
     .catch(fail)
+})
+
+test('patch /users/aaaaa - not found', ()=>{
+  return request(address)
+          .patch(`/users/aaaaa`)
+          .then(response=>{
+                expect(response.status).toBe(404)
+           }).catch(fail)
+})
+
+test('put /users/aaaaa - not found', ()=>{
+  return request(address)
+          .put(`/users/aaaaa`)
+          .then(response=>{
+                expect(response.status).toBe(404)
+           }).catch(fail)
+})
+
+/*
+  1. Cria-se um usuario com gender Male
+  2. Atualiza, mas nao informa gender
+  3. Testa se o documento foi substituido -> gender undefined
+*/
+test('put /users:/id', ()=>{
+  return request(address)
+            .post('/users')
+            .send({
+              name: 'usuario 7',
+              email: 'user7@gmail.com',
+              password: '123456',
+              cpf: '613.586.318-59',
+              gender: 'Male'
+            }).then(response => request(address)
+                     .put(`/users/${response.body._id}`)
+                     .send({
+                       name: 'usuario 7',
+                       email: 'user7@gmail.com',
+                       password: '123456',
+                       cpf: '613.586.318-59'
+                     }))
+              .then(response=>{
+                       expect(response.status).toBe(200)
+                       expect(response.body.name).toBe('usuario 7')
+                       expect(response.body.email).toBe('user7@gmail.com')
+                       expect(response.body.cpf).toBe('613.586.318-59')
+                       expect(response.body.gender).toBeUndefined()
+                       expect(response.body.password).toBeUndefined()
+            }).catch(fail)
+
+})
+
+test('delete /users/qqqq - not found', () => {
+  return request(address)
+  .get('/users/aaaa')
+  .then(response => {
+    expect(response.status).toBe(404)
+  }).catch(fail)
+})
+
+test('delete /users/:id', () => {
+  return request(address)
+  .post('/users')
+  .send({
+    name: 'usuario 3',
+    email: 'user3@gmail.com',
+    password: '123456',
+    cpf: '187.638.581-26'
+  }).then(response => request(address)
+           .delete(`/users/${response.body._id}`))
+    .then(response=>{
+      expect(response.status).toBe(204)
+ }).catch(fail)
 })
